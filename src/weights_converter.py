a4a286c89722c087964a972ff13926781819f662
AakashKumarNain <aakashnain@outlook.com>
2024-05-01 16:37:18 +0530

    save ndarray dict to pickle

--- src/weights_converter.py
+++ src/weights_converter.py
@@ -2,19 +2,25 @@ import pickle
 import torch
 import numpy as np
 from keras import ops
+from pathlib import Path
+
 
 # Note: Using pickle because I am just being lazy. There are better alternatives!
-
-def save_torch_weights_as_numpy_arrays(state_dict):
+def save_torch_weights_as_numpy_arrays(state_dict, save_path):
     """"Convert torch weights to numpy and save them as `ndarrays`.
 
     Args:
         state_dict: Ordered dict containing the weights of Mistral-7B
             model represented in fp16 format.
+        save_path: Path to save the pickle file.
     """
 
     if state_dict is None:
         raise ValueError(f"Expected an OrderedDict. Received {state_dict}.")
+    
+    save_path = Path(save_path)
+    if save_path.suffix != ".pkl":
+        raise ValueError(f"Expected `save_path` to have `.pkl` extension")
 
     new_state_dict = {}
 
@@ -25,7 +31,14 @@ def save_torch_weights_as_numpy_arrays(state_dict):
                 new_state_dict[key] = state_dict[key].cpu().numpy().T
             else:
                 new_state_dict[key] = state_dict[key].cpu().numpy()
+
+        with open(save_path, "wb") as f:
+            pickle.dump(new_state_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
+    
     except Exception as e:
         print("Oh oh! Something went wrong!\n", e)
 
 
+
+
+def convert_numpy_weights
